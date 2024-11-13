3e3cb8850f280c091a15274a81ebd40ce87d60a6
lijiaze <3035500479@qq.com>
2024-11-13 17:51:42 +0800

    change work_func

--- include/ThreadPool.hpp
+++ include/ThreadPool.hpp
@@ -34,6 +34,8 @@ public:
     explicit thread_pool( const size_t threads_count = std::thread::hardware_concurrency() - 1 ) : threads_count_( threads_count ) {
         threads_.resize( threads_count );
         create_threads( threads_count_ );
+        manager_ = std::thread( &thread_pool::manage_func, this );
+        manager_.detach();
     }
 
     ~thread_pool() {
@@ -47,9 +49,14 @@ public:
         }
         std::unique_lock< std::mutex > ul( mutex_ );
         state_ = pool_state::waitting;
-        done_cond_.wait( ul, [ this ]() { return tasks_.empty(); } );
+        done_cond_.wait( ul, [ this ]() {
+            // std::cout << "wait_util_done" << std::endl;
+            return tasks_.empty();
+        } );
+        // std::cout << "done" << std::endl;
     }
     void destroy_pool() {
+        state_ = pool_state::closed;
         cond_.notify_all();
         tasks_.get_cond().notify_all();
         std::for_each( threads_.begin(), threads_.end(), []( std::thread& t ) {
@@ -57,6 +64,7 @@ public:
                 t.join();
             }
         } );
+        // std::cout << "destroy ok" << std::endl;
     }
 
     template < typename Func, typename... Args >
@@ -68,19 +76,17 @@ public:
 
         typename task_queue::value_type tt( [ = ]() { ( *task )(); } );
         tasks_.push( std::move( tt ) );
+        cond_.notify_one();
         return task->get_future();
     }
 
-    inline void run() {
+    inline void resume() {
         state_ = pool_state::running;
         cond_.notify_all();
     }
 
     inline bool is_closed() const noexcept {
         return state_ == pool_state::closed;
-    }
-    inline void close() {
-        state_ = pool_state::closed;
     }
 
     inline bool is_paused() const noexcept {
@@ -97,6 +103,7 @@ public:
 
 private:
     inline void create_threads( const size_t n ) {
+        state_ = pool_state::running;
         std::transform( threads_.begin(), threads_.end(), threads_.begin(), [ this ]( std::thread& t ) {
             t = std::thread( &thread_pool::work_func, this );
             return std::move( t );
@@ -106,16 +113,26 @@ private:
     void work_func() {
         for ( ;; ) {
             std::unique_lock< std::mutex > ul( mutex_ );
-            cond_.wait( ul, [ this ]() { return !is_paused(); } );
-            if ( is_closed() ) {
+            cond_.wait( ul, [ this ]() { return ( is_closed() || !tasks_.empty() ) && !is_paused(); } );
+            if ( is_closed() && tasks_.empty() ) {
+                // std::cout << "thread id :" << std::this_thread::get_id() << "break loop" << std::endl;
                 break;
             }
 
             auto task = tasks_.pop();
+            ul.unlock();
             task();
+
             if ( state_ == pool_state::waitting && tasks_.empty() ) {
                 done_cond_.notify_all();
             }
+        }
+    }
+
+    void manage_func() {
+        for ( ;; ) {
+            std::cout << "i am manager thread" << std::endl;
+            std::this_thread::sleep_for( std::chrono::seconds( 2 ) );
         }
     }
 
